acc9dab19334824e389dfb328c38a48cfa1f798a
BadmaU <61361339+BadmaU@users.noreply.github.com>
2020-07-20 17:28:58 +0300

    Update RomanNumber.java

--- src/RomanNumber.java
+++ src/RomanNumber.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class RomanNumber extends ArabicNumber
 {
-        static String[] romanNums = new String [] {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        static int lenNums = romanNums.length;
+    static String[] romanNums = new String [] {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    static int lenNums = romanNums.length;
 
     public static boolean isInputValueIsNumber (String value)
     {
@@ -11,18 +11,28 @@ public class RomanNumber extends ArabicNumber
     }
 
     static String calculateRomanNumbers (String value1, String value2, String operator) {
-            for (int i = 0; i < lenNums; i++) {
-                if ( i == 10)
-                    break;
-            }
+        for (int i = 0; i < lenNums; i++) {
+            if ( i == 10)
+                break;
+        }
         String value1toArabic = Integer.toString(Arrays.binarySearch(romanNums, value1) + 1);
         String value2toArabic = Integer.toString(Arrays.binarySearch(romanNums, value2) + 1);
 
-        int resultArabic = ArabicNumber.calculateArabicNumber(value1toArabic, value2toArabic, operator);
+        int resultArabic = calculateArabicNumber(value1toArabic, value2toArabic, operator);
 
         String result = null;
 
         return result;
     }
-}
 
+    public static int findIndex(String[] list, String value)
+    {
+        int len = list.length;
+        int index = 0;
+        for (int i=0; i < len; i++) {
+            if (list[i].equals(value))
+                index = i;
+        }
+        return index;
+    }
+}
